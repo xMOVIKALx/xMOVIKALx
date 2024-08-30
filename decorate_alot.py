088c3959c9deb733e37d4539fa6c545e3bfa947b
xMOVIKALx <ahmadmovahed89@gmail.com>
2024-08-30 12:57:43 +0330

    V2
    Changes :
    1 . module name decorator -> decorate_alot
    2 . added red_print function
    3 . some bugs fixed

---
+++ decorate_alot.py
@@ -0,0 +1,99 @@
+# This is a simple-function-wrapper for prints
+# You can use it just for print-in functions
+# like :
+# ------
+# decorator = Wrapper()
+#
+# @decorator.custom_symbol('+', max_digits=10)
+# def p():
+#   print("Hello world!")
+# -----------------------------------------------
+# output :
+# ++++++++++
+# Hello world!
+# ++++++++++
+import sys
+
+
+def red_print(value):
+    print(value, file=sys.stderr)
+
+
+class Wrapper:
+
+    def custom_symbol(self, symbol, max_digits=100, space=False):
+        if space == True:
+            symbol = f"{symbol} "
+
+        def wrapper(func):
+            print(f'{symbol}' * max_digits)
+            func()
+            print(f'{symbol}' * max_digits)
+
+        return wrapper
+
+    def line(self, max_digits=100, space=False):
+        if space == True:
+            symbol = f"- "
+
+        def wrapper(func):
+            print(f'{symbol}' * max_digits)
+            func()
+            print(f'{symbol}' * max_digits)
+
+        return wrapper
+
+    def star(self, max_digits=100, space=False):
+        if space == True:
+            symbol = f"* "
+
+        def wrapper(func):
+            print(f'{symbol}' * max_digits)
+            func()
+            print(f'{symbol}' * max_digits)
+
+        return wrapper
+
+    def arrow(self, max_digits=100, space=False):
+        if space == True:
+            symbol = f"^ "
+
+        def wrapper(func):
+            print(f'{symbol}' * max_digits)
+            func()
+            print(f'{symbol}' * max_digits)
+
+        return wrapper
+
+    def square(self, max_digits=100, space=False):
+        if space == True:
+            symbol = f"# "
+
+        def wrapper(func):
+            print(f'{symbol}' * max_digits)
+            func()
+            print(f'{symbol}' * max_digits)
+
+        return wrapper
+
+    def tilde(self, max_digits=100, space=False):
+        if space == True:
+            symbol = f"~ "
+
+        def wrapper(func):
+            print(f'{symbol}' * max_digits)
+            func()
+            print(f'{symbol}' * max_digits)
+
+        return wrapper
+
+    def dot(self, max_digits=100, space=False):
+        if space == True:
+            symbol = f". "
+
+        def wrapper(func):
+            print(f'{symbol}' * max_digits)
+            func()
+            print(f'{symbol}' * max_digits)
+
+        return wrapper
